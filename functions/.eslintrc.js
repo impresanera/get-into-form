91c164f57a8be3ee5ea7c3d1994d6b6c903390c7
Micah Effiong <micaiah.effiong@gmail.com>
2023-04-04 16:55:06 +0100

    resolved prettier formatting

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,32 +1,35 @@
-{
-  "root": true,
-  "env": {
-    "es6": true,
-    "node": true
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
   },
-  "extends": [
+  extends: [
     "eslint:recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript",
     "google",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": ["tsconfig.json", "tsconfig.dev.json"],
-    "tsconfigRootDir": ".",
-    "sourceType": "module"
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
   },
-  "ignorePatterns": [
-    "/lib/**/*" // Ignore built files.
+  ignorePatterns: [
+    "/lib/**/*", // Ignore built files.
   ],
-  "plugins": ["@typescript-eslint", "import"],
-  "rules": {
-    "quotes": ["error", "double"],
+  plugins: ["@typescript-eslint", "import", "prettier"],
+
+  rules: {
+    "prettier/prettier": ["error"],
+    quotes: ["error", "double"],
     "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    indent: ["error", 2],
     "object-curly-spacing": 0,
-    "camelcase": "warn"
-  }
-}
+    camelcase: "warn",
+  },
+};
