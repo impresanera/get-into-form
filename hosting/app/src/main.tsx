c5e8df90cf61d46a7464ee119ca074130e9eed41
Micah Effiong <micaiah.effiong@gmail.com>
2023-04-16 19:01:23 +0100

    remove export for query-cliient

--- hosting/app/src/main.tsx
+++ hosting/app/src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
