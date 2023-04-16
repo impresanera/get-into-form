7b41eb78d9755adbf0076b4de8bfd87b047955c5
Micah Effiong <micaiah.effiong@gmail.com>
2023-04-17 00:53:23 +0100

    configure function to run based on env

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -5,8 +5,17 @@ const env = {
   testing: "uat", // user assertion test
 } as const;
 
-export const appendEnv = <T extends string>(
-  str: T
-): `${T}_${(typeof env)[typeof process.env.NODE_ENV]}` => {
+type ENV = typeof env;
+type EnvKeys = ENV[keyof ENV];
+
+export const appendEnv = <T extends string>(str: T): `${T}_${EnvKeys}` => {
   return `${str}_${env[process.env.NODE_ENV]}`;
+};
+
+export const runInEnv = <T>(envKey: EnvKeys, fn: T) => {
+  if (envKey === env[process.env.NODE_ENV]) {
+    return fn;
+  } else {
+    return () => "";
+  }
 };
