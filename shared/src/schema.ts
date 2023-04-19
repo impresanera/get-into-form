80e2758673b5c4cee890ea045d8fec8993119750
Micah Effiong <micaiah.effiong@gmail.com>
2023-04-19 14:45:11 +0100

    updated shared files

--- shared/src/schema.ts
+++ shared/src/schema.ts
@@ -1,43 +1,65 @@
 import { z } from "zod";
 
-const isRequiredMessage = (name: string): string => `[${name} is required]`;
+const isRequiredMessage = (name: string): string => `[${name}] is required`;
 
 export const createMailgunFormAutomation = z.object({
   type: z.enum(["EMAIL"], { required_error: isRequiredMessage("type") }),
   provider: z.enum(["MAILGUN"], {
     required_error: isRequiredMessage("provider"),
   }),
-  apiKey: z.string({ required_error: isRequiredMessage("apiKey") }),
-  domain: z.string({ required_error: isRequiredMessage("domain") }),
+  apiKey: z
+    .string({ required_error: isRequiredMessage("apiKey") })
+    .trim()
+    .min(1),
+  domain: z
+    .string({ required_error: isRequiredMessage("domain") })
+    .trim()
+    .min(1),
   sender: z
     .string({
       required_error: isRequiredMessage("sender"),
     })
+    .trim()
+    .min(1)
     .regex(/^[a-zA-Z]+(\s[a-zA-Z]+)?\s<.+>/, {
-      message: "[sender] is invalide. Use the email name format 'name <email>'",
+      message: "[sender] is invalid. Use the email name format 'name <email>'",
     }),
-  receiverType: z.enum(["FIXED", "FORM_DATA"], {
+  receiverSource: z.enum(["FIXED", "FORM_DATA"], {
     required_error: isRequiredMessage("receiverType"),
   }),
-  receiver: z.string({
-    required_error: isRequiredMessage("receiver"),
-  }),
+  receiverValue: z
+    .string({
+      required_error: isRequiredMessage("receiver"),
+    })
+    .trim()
+    .min(1),
   emailSourceType: z.enum(["TEMPLATE"], {
     required_error: isRequiredMessage("emailSourceType"),
   }),
-  emailSourceValue: z.string({
-    required_error: isRequiredMessage("emailSourceValue"),
-  }),
-  name: z.string({ required_error: isRequiredMessage("name") }),
-  emailSubject: z.string({
-    required_error: isRequiredMessage("emailSubject"),
-  }),
+  emailSourceValue: z
+    .string({
+      required_error: isRequiredMessage("emailSourceValue"),
+    })
+    .trim()
+    .min(1),
+  name: z
+    .string({ required_error: isRequiredMessage("name") })
+    .trim()
+    .min(1),
+  emailSubject: z
+    .string({
+      required_error: isRequiredMessage("emailSubject"),
+    })
+    .trim()
+    .min(1),
   replyTo: z
     .string({ required_error: isRequiredMessage("replyTo") })
+    .trim()
+    .min(1)
     .optional()
     .nullable()
     .default(null),
-  trigger: z.array(z.enum(["ON_DATA_ADDED"])).nonempty(),
+  trigger: z.enum(["ON_DATA_ADDED"]).array().nonempty(),
   isEu: z.boolean().optional(),
 });
 // .refine((val) => {
